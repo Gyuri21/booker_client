f00b9ee6b2f5a94da2597b2812c49f1cbcfca9eb
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-06 12:09:20 +0100

    works

---
+++ src/app/api.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { enableDebugTools } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  host = 'http://localhost:3000/'
+  constructor(private http:HttpClient) { }
+  
+  getBooks(){
+    let endpoint = 'books';
+    let url = this.host + endpoint;
+    return this.http.get<any>(url);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { BooksComponent } from './books/books.component';
+BooksComponent
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
